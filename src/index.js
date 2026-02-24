72d0fb900ccba987b45eda6baaf28d1475896e8c
Nivas Reddy <bn123@snu.edu.in>
2026-02-25 00:37:25 +0530

    added health checup

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { initDB } = require('./db');
+const { identifyContact } = require('./identify');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => { res.json({ status: 'looks good' });});
+
+app.post('/identify', async (req, res) => {
+  try {
+    const email = req.body.email?.trim() || null;
+    const phoneNumber = req.body.phoneNumber?.toString().trim() || null;
+
+    if (!email && !phoneNumber) {
+      return res.status(400).json({ error: 'Please provide at least an email or phone num bro.' });
+    }
+
+    const response = await identifyContact(email, phoneNumber);
+    return res.status(200).json(response);
+  } catch (err) {
+    console.error('Error in enpoint nigga:', err);
+    return res.status(500).json({ error: 'Internal server error.' });
+  }
+});
+
+const PORT = process.env.PORT;
+
+initDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to initialize database:', err);
+    process.exit(1);
+  });
